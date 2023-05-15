da4074e8eca8b0b8dcf6df87b506a8daf61cac93
bihan.hj <bihan.hj@alibaba-inc.com>
2023-05-16 00:37:40 +0800

    test

--- src/main/java/com/yes/TestWatch.java
+++ src/main/java/com/yes/TestWatch.java
@@ -1,12 +1,14 @@
 package com.yes;
 
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.io.IOException;
 import java.util.List;
 
-public class TestWatch {
+public class TestWatch implements Watcher {
 
     // zookeeper 地址, 多个地址以逗号割开
     private static String zkServer = "127.0.0.1:2181";
@@ -14,21 +16,50 @@ public class TestWatch {
     // 会话超时时间
     private static int sessionTimeout = 6000;
 
+    ZooKeeper zooKeeper;
+
 
     public static void main(String[] args) throws Exception {
-        // 创建zookeeper 连接时, 设置全局监听器
-        ZooKeeper zooKeeper = new ZooKeeper(zkServer, sessionTimeout, watchedEvent -> {
-            // 获取发生事件的节点路径
-            String path = watchedEvent.getPath();
-            System.out.println(path);
 
-        });
-
-        // 一旦注册便会一直监听,只要有变化, 就会通知全局监听器
-        zooKeeper.getChildren("/", true);
+        TestWatch testWatch = new TestWatch();
+        testWatch.connect();
+        testWatch.watch("/demo");
 
         // 线程休眠, 否则不能监控到数据
         Thread.sleep(Long.MAX_VALUE);
     }
 
+    private void connect() throws IOException {
+        this.zooKeeper = new ZooKeeper(zkServer, sessionTimeout, this);
+    }
+
+    private void watch(String path) throws InterruptedException, KeeperException {
+        System.out.println("监视节点" + path);
+        List<String> children = this.zooKeeper.getChildren(path, true);
+        System.out.println("子节点有" + children.toString());
+    }
+
+
+    @Override
+    public void process(WatchedEvent event) {
+        System.out.println("监视节点" + event.getPath());
+        List<String> children = null;
+        try {
+            children = this.zooKeeper.getChildren(event.getPath(), true);
+        } catch (KeeperException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("子节点有" + children.toString());
+        for (String child : children) {
+            try {
+                this.zooKeeper.getChildren(event.getPath() + "/" + child, true);
+            } catch (KeeperException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
